0012d287d6399549cbd386596a577157a6a5884a
dahlki <dahlki78@gmail.com>
2017-09-20 19:42:05 -0400

    chore(yarn, babel): update yarn and babel-loader-env

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
-          presets: ['react', 'es2015', 'stage-3']
+          presets: ['react', 'env', 'stage-3']
         }
       },
       {
